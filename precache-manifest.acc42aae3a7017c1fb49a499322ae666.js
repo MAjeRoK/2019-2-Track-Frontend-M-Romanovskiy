7b7781edd0596c6fcc0ffed14c30d0af8c14352e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-06 18:31:53 +0000

    Deploy MAjeRoK/2019-2-Track-Frontend-M-Romanovskiy to github.com/MAjeRoK/2019-2-Track-Frontend-M-Romanovskiy.git:gh-pages

---
+++ precache-manifest.acc42aae3a7017c1fb49a499322ae666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c6e47e48199ae09386adcf99a81b28",
+    "url": "/2019-2-Track-Frontend-M-Romanovskiy/index.html"
+  },
+  {
+    "revision": "b7b09245e8c512a7071c",
+    "url": "/2019-2-Track-Frontend-M-Romanovskiy/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "dfcc8966d7f29ee64ae1",
+    "url": "/2019-2-Track-Frontend-M-Romanovskiy/static/js/2.273bc32e.chunk.js"
+  },
+  {
+    "revision": "b7b09245e8c512a7071c",
+    "url": "/2019-2-Track-Frontend-M-Romanovskiy/static/js/main.7fe08ebb.chunk.js"
+  },
+  {
+    "revision": "d165a125c13d2dacc06c",
+    "url": "/2019-2-Track-Frontend-M-Romanovskiy/static/js/runtime-main.132eff57.js"
+  },
+  {
+    "revision": "795fb555bea801821f51202dc10aaa78",
+    "url": "/2019-2-Track-Frontend-M-Romanovskiy/static/media/logo.795fb555.svg"
+  }
+]);
